69e4a99484396616f10caa07dfef9046c4f2fb3d
churchwardpumpkinpatch <churchwardpumpkinpatch@gmail.com>
2025-10-22 15:03:46 -0700

    ,,/,,

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
-  
-
-
-  // List of files in your gallery
-// For now, you can hardcode some examples
+// List of PDF files (assume PDFs are in 'assets/' folder)
 const files = [
+  'assets/AGreatPairOfShoes.pdf',
+  'assets/AGreatPairOfShoes.pdf',
   'assets/AGreatPairOfShoes.pdf',
   'assets/AGreatPairOfShoes.pdf',
   'assets/AGreatPairOfShoes.pdf',
@@ -16,50 +13,39 @@ const files = [
 const gallery = document.getElementById('gallery');
 
 files.forEach(file => {
-  // Container for each item + button
   const container = document.createElement('div');
   container.classList.add('item-container');
 
-  // Display PDF or image
-  if (file.endsWith('.pdf')) {
-    const img = document.createElement('img');
-    img.src = `https://docs.google.com/gview?embedded=true&url=${location.origin}/${file}`;
-    img.alt = file;
-    img.classList.add('gallery-item');
-    container.appendChild(img);
-  } else {
-    const img = document.createElement('img');
-    img.src = file;
-    img.alt = file;
-    img.classList.add('gallery-item');
-    container.appendChild(img);
-  }
-  
+  // Display PDF as an image preview
+  // Assumes you have corresponding PNG/JPG with same name
+  const previewFile = file.replace('.pdf', '.png'); 
+  const img = document.createElement('img');
+  img.src = previewFile;
+  img.alt = file;
+  img.classList.add('gallery-item');
+  container.appendChild(img);
 
   // Create Print button
   const btn = document.createElement('button');
   btn.textContent = 'Print';
   btn.classList.add('print-btn');
 
-  btn.onclick = () => printFile(file);
+  // Print original PDF
+  btn.addEventListener('click', () => {
+    const newTab = window.open(file, '_blank');
+    if (newTab) {
+      const checkReady = setInterval(() => {
+        if (newTab.document && newTab.document.readyState === 'complete') {
+          clearInterval(checkReady);
+          newTab.focus();
+          newTab.print();
+        }
+      }, 300);
+    } else {
+      alert('Please allow pop-ups to enable printing.');
+    }
+  });
 
   container.appendChild(btn);
   gallery.appendChild(container);
 });
-
-function printFile(file) {
-  const newTab = window.open(file, '_blank');
-
-  // wait until PDF is ready before triggering print
-  if (newTab) {
-    const checkReady = setInterval(() => {
-      if (newTab.document && newTab.document.readyState === 'complete') {
-        clearInterval(checkReady);
-        newTab.focus();
-        newTab.print();  // open native print dialog
-      }
-    }, 300);
-  } else {
-    alert('Please allow pop-ups to enable printing.');
-  }
-}
